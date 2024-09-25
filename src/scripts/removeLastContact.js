f2e58fbf9050c1538f762fdce08919619d36e868
Oleksandr-Kalinin <work.kalinin.o.v@gmail.com>
2024-09-25 23:22:36 +0300

    hw1

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,13 +1,26 @@
+import { PATH_DB } from "../constants/contacts.js";
 import { readContacts } from "../utils/readContacts.js";
-import { writeContacts } from "../utils/writeContacts.js";
-import { removeAllContacts } from "./removeAllContacts.js";
+// import { removeAllContacts } from "./removeAllContacts.js";
+// import { writeContacts } from "../utils/writeContacts.js";
+import fs from 'node:fs/promises';
+
 
 export const removeLastContact = async () => {
-    const data = await readContacts();
-    if (data.length === 0) return console.log("File is empty");
-    data.pop();
-    await removeAllContacts();
-    await writeContacts(data);
+
+    try {
+        const data = await readContacts();
+        if (data.length === 0) return console.log("File is empty");
+        data.pop();
+
+        // Цей варіант не працює:
+        // await removeAllContacts();
+        // await writeContacts(data);
+
+        await fs.writeFile(PATH_DB, JSON.stringify([...data]), 'utf8');
+    } catch (error) {
+        console.error('Error removing the last element:', error);
+    }
+
 };
 
 removeLastContact();
